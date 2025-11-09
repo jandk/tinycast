c7d3b49adc44e7500e72da0de4964c58d5195c38
Jan De Kock <jan@twofold.be>
2025-11-09 21:18:22 +0100

    Make CastException extend IOException for easier handling

--- src/main/java/be/twofold/tinycast/CastException.java
+++ src/main/java/be/twofold/tinycast/CastException.java
@@ -1,12 +1,14 @@
 package be.twofold.tinycast;
 
+import java.io.IOException;
+
 /**
  * Exception indicating a problem encountered while processing Cast data.
  * <p>
  * This exception is thrown when reading or writing Cast streams fails due to
  * malformed input, unknown identifiers, or other format-related issues.
  */
-public final class CastException extends Exception {
+public final class CastException extends IOException {
     /**
      * Creates a new exception with the specified detail message.
      *
